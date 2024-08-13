667355eb104b885191243eb92d966952b3f8d4e7
NicolausFeldtmann <n.feldtmann@yahoo.dem>
2024-08-13 21:23:44 +0200

    DB added

---
+++ dbase.js
@@ -0,0 +1,25 @@
+let menu = [
+    {
+        "name": "Leonardo",
+        "ingredients": "Leckere Deepdish Pizza! Mit Bananen und Würstechen. Auch vegan erhältlich!",
+        "price": 15.00,
+    },
+
+    {
+        "name": "Donatello",
+        "ingredients": "Super dünner Boden! belget mit Kidneybohnen und Waldpilzen!",
+        "price": 16.00,
+    },
+
+    {
+        "name": "Raphael",
+        "ingredients": "Die beste Pizza für den Sommer! Peperonisalami und Vanilleeis Am besten schnell Vverputzen!",
+        "price": 14.50, 
+    },
+
+    {
+        "name": "Michelangelo",
+        "ingredients": "Mit leckerem Käse im Rand! Eingelegt Sardellen und schelzender Karamell Ein gute kombination aus süß und Salzig!",
+        "price": 15.50,
+    },
+];
